fa281e721d7eaa5dbf844550414891f1f51d58f2
shivam gurunani <namisha.gurunani173261@gmail.com>
2023-11-14 05:28:53 +0530

    payment integarton added

---
+++ Namisha_Payment-integration/products.js
@@ -0,0 +1,600 @@
+const products = [
+
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/6/7/677754dDAIAA00000256_10.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Daily Life Forever52 Pro Artist Ultra Definition Liqu...",
+    
+    rating: "★★★★★",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 499.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 449.99,
+    
+    discountPrice: 50.00,
+    
+    popularity: 200,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/9/4/94aca07nyswissb00144_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Swiss Beauty Metallic Liquid Eyeshadow - 06 Molten Midnight",
+    
+    rating: "★★★★☆",
+    
+    flipkartAssured: false,
+    
+    shades: 12,
+    
+    price: 299.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 279.99,
+    
+    discountPrice: 20.00,
+    popularity: 100,
+    
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/6/2/6291106034271_1.jpg",
+    
+    title: "Huda Beauty Nude Obsessions Mini Eyeshadow Palette",
+    
+    rating: "★★★★★",
+    
+    flipkartAssured: true,
+    
+    shades: 3,
+    
+    price: 699.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 599.99,
+    
+    discountPrice: 100.00,
+    popularity: 500,
+    
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/0/0/0053420NYKAC00000072_1new.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Nykaa Matte Waterproof, Transfer Proof, Liquid Lipstick",
+    
+    rating: "★★★☆☆",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 199.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 179.99,
+    
+    discountPrice: 20.00,
+    popularity: 900,
+    
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/7/4/7439656LOVEE00000117_1a.jpg",
+    
+    title: "Love Earth Liquid Mousse Liquid Matte Lipstick 1.0",
+    
+    rating: "★★★☆☆",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 899.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 849.99,
+    
+    discountPrice: 50.00,
+    popularity: 10,
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/0/e/0e16918NYLAGRL000033_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "L.A Girl HD Pro Conceal - Orange Corrector",
+    
+    rating: "★★★★★",
+    
+    flipkartAssured: false,
+    
+    shades: 12,
+    
+    price: 399.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 359.99,
+    
+    discountPrice: 40.00,
+    
+    popularity: 50,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/2/8/2807eca24859_H_8901030794773.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Lakme 9 To 5 Primer + Matte Lipstick - MP8 Rosy Sunday",
+    
+    rating: "★★★★★",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 599.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 549.99,
+    
+    discountPrice: 50.00,
+    popularity: 1000,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/6/6/66ab48b8906087778288_1.jpg",
+    
+    title: "Mamaearth Glow Serum Foundation with Vitamin C ",
+    
+    rating: "★★★★★",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 349.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: false,
+    
+    specialPrice: 329.99,
+    
+    discountPrice: 20.00,
+    popularity: 2,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/0/2/0274c23716170284699_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Bobbi Brown Vitamin Enriched Skin Tint - Light 1",
+    
+    rating: "★★★★★",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 799.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 749.99,
+    
+    discountPrice: 50.00,
+    popularity: 34634,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/5/5/555cfc48904245705107_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Nykaa Matte to Last! Transfer Proof Liquid Lipstick - Boho 16",
+    
+    rating: 4.6,
+    
+    flipkartAssured: false,
+    
+    shades: 12,
+    
+    price: 249.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 229.99,
+    
+    discountPrice: 20.00,
+    popularity: 9800,
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/e/b/eb92535BlueHeaven001_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Blue Heaven Eye Combo - Sketch Eyeliner",
+    
+    rating: "★★★★☆",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 599.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 579.99,
+    
+    discountPrice: 20.00,
+    popularity: 201,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/5/a/5a234c0651986971961ab_0.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Too Faced Better Than Sex Mascara Travel Size - Black",
+    
+    rating: "★★★★☆",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 179.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 169.99,
+    
+    discountPrice: 10.00,
+    popularity: 199,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/0/8/08329e3RENEE00000450_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Renee Cosmetics Bollywood Filter Face Primer",
+    
+    rating: "★★★★★",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 899.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 849.99,
+    
+    discountPrice: 50.00,
+    popularity: 2300,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/e/b/eb92535BlueHeaven003_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Blue Heaven Eye Combo - Lash Twist Curling Mascara",
+    
+    rating: "★★★★☆",
+    
+    flipkartAssured: false,
+    
+    shades: 12,
+    
+    price: 449.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 429.99,
+    
+    discountPrice: 20.00,
+    popularity: 990,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/d/d/dd8ce0b192333078259_01.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Clinique Even Better Clinical Serum Foundation",
+    
+    rating: "★★★☆☆",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 299.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 269.99,
+    
+    discountPrice: 30.00,
+    popularity: 1000,
+    
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/0/2/02be2108904245700461_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Nykaa Nail Enamel Polish - Peppermint-ini 60",
+    
+    rating: "★★★★☆",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 699.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 649.99,
+    
+    discountPrice: 50.00,
+    popularity: 0,
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/3/c/3c8850bSUGAG00000048_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "SUGAR POP Nail Lacquer - 08 Silk Stockings",
+    
+    rating: "★★★★★",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 549.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 499.99,
+    
+    discountPrice: 50.00,
+    popularity: 240,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/7/f/7f073f68904245703240_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Nykaa Get Cheeky! Blush Duo palette - Malibu Barbie 03",
+    
+    rating: "★★★★★",
+    
+    
+    flipkartAssured: false,
+    
+    shades: 12,
+    
+    price: 499.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 469.99,
+    
+    discountPrice: 30.00,
+    popularity: 560,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/7/5/75e4ee6JUSTH00000276_1.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Just Herbs Nail Polish Quick Dry Glossy Nail Paint",
+    
+    rating: "★★★★☆",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 349.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 329.99,
+    
+    discountPrice: 20.00,
+    popularity: 299,
+    
+    },
+    
+    {
+    
+    image: "https://images-static.nykaa.com/media/catalog/product/5/2/528e0c58904245705206_0.jpg?tr=w-344,h-344,cm-pad_resize",
+    
+    title: "Nykaa Glow Highlighting & Illuminating Palette",
+    
+    rating: "★★★★★",
+    
+    flipkartAssured: true,
+    
+    shades: 12,
+    
+    price: 649.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 599.99,
+    
+    discountPrice: 50.00,
+    popularity: 200,
+    
+    },
+    
+    ];
+    const cart = [];
+
+    function addToCart(productTitle, productPrice) {
+        cart.push({ title: productTitle, price: productPrice });
+        updateCart();
+    }
+
+    function updateCart() {
+        const cartCount = document.getElementById('cart-count');
+        const cartItems = document.getElementById('cart-items');
+
+        // Update cart count
+        cartCount.textContent = cart.length;
+
+        // Clear existing cart items
+        cartItems.innerHTML = '';
+
+        // Display cart items in the cart page
+        cart.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = `${item.title} - Rs. ${item.price}`;
+            cartItems.appendChild(li);
+        });
+    }
+
+    // Function to open the cart page
+    function openCartPage() {
+        updateCart();
+        const cartPage = document.getElementById('cart-page');
+        cartPage.style.display = 'flex';
+    }
+
+    // Function to close the cart page
+    function closeCartPage() {
+        const cartPage = document.getElementById('cart-page');
+        cartPage.style.display = 'none';
+    }
+    
+            // Function to render product containers
+            function renderProducts() {
+                const productsContainer = document.getElementById('products-container');
+    
+                products.forEach(product => {
+                    const container = document.createElement('div');
+                    container.classList.add('product-container');
+    
+                    // Add product details to the container
+                    container.innerHTML = `
+                        <img src="${product.image}" alt="${product.title}">
+                        <h2>${product.title}</h2>
+                        <p>${product.rating}</p>
+                        <p class="shades">${product.shades} shades</p>
+                        <span class="special"> Rs. ${product.specialPrice}<p class="price">Rs. ${product.price}</p></span>
+                        <button onclick="handlePayment('${product.title}', ${product.specialPrice}, '${product.image}')">Buy Now</button>
+                        <button class="cart" onclick="addToCart('${product.title}', ${product.specialPrice})">Add to Cart <i class="fa-solid fa-cart-shopping" id="cart-icons"></i></button>
+                    `;
+    
+                    productsContainer.appendChild(container);
+                });
+            }
+    
+            // Function to handle payment using Razorpay
+            function handlePayment(productTitle, productPrice, productImage) {
+                const options = {
+                    key: 'rzp_test_DhnX2ljNSBBudR', // Replace with your Razorpay API key
+                    amount: productPrice * 100, // Amount in paisa
+                    currency: 'INR',
+                    name: productTitle,
+                    description: `Payment for ${productTitle}`,
+                    image: productImage,
+                    handler: function (response) {
+                        alert(`Payment successful for ${productTitle}!`);
+                        // Implement logic to update cart, etc.
+                    },
+                    prefill: {
+                        name: 'Customer Name',
+                        email: 'customer@example.com',
+                        contact: 'XXXXXXXXXX',
+                    },
+                    notes: {
+                        address: 'Customer Address',
+                    },
+                    theme: {
+                        color: '#F37254', // Replace with your desired color
+                    },
+                };
+    
+                const rzp = new Razorpay(options);
+                rzp.open();
+            }
+    
+            // Call the renderProducts function to display product containers
+            renderProducts();
