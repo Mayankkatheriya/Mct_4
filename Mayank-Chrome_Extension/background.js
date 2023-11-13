33d3f056746e523f1c1cf1704c1bb3631b06a752
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-11-14 02:11:25 +0530

    bacground js done

--- Mayank-Chrome_Extension/background.js
+++ Mayank-Chrome_Extension/background.js
@@ -1,7 +1,4 @@
-console.log("hiii");
-
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-
   if (tab.url && tab.url.includes("youtube.com/watch")) {
     const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
---
+++ Mayank-Chrome_Extension/popup.js
@@ -0,0 +1,1 @@
+import { getActiveTabURL } from "./utlis.js";
