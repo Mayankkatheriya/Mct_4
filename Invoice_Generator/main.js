83a912304704f174fbba796d608415d498876be9
Dibyas98 <dibyashaktip98.com>
2023-11-13 20:35:00 +0530

    function added

--- Invoice_Generator/main.js
+++ Invoice_Generator/main.js
@@ -1,20 +1,46 @@
 let bill = document.querySelectorAll('.bill');
 let first= document.querySelector('.page')
-console.log(first);
-console.log(bill);
+let c=2;
+// console.log(first);
+// console.log(bill);
+
+
+
+function shift_Price(){
+    let name=document.querySelectorAll('.name');
+    name.forEach((ele) =>{
+        ele.addEventListener('keyup',function(e){
+            if(e.key==='Enter' && ele.value.length>0){
+                ele.parentElement.children[2].focus();
+            }
+            
+        })
+    })
+}
+shift_Price();
 
 function add_Entery(){
     let price=document.querySelectorAll('.price');
     price.forEach(element => {
         element.addEventListener('keyup',function(e){
-            if(e.key === 'Enter'){
+            if(e.key === 'Enter' && element.value.length>0){
+                element.disabled=true
                 let newDiv=document.createElement('div')
+                newDiv.classList.add('bill')
             newDiv.innerHTML=`
-            <input type="text">
+            <p>${c}</p>
+            <input type="text"  class="name">
             <input type="number" class="price">
             `
+            c++;
             first.append(newDiv)
+            let l=first.children.length
+            first.children[l-1].children[1].focus();
+            // console.log(first.children[l-1].children[1]);
+            // console.log(first.children.length);
+            total(price);
             add_Entery();
+            shift_Price();
 
             }
             
@@ -22,3 +48,16 @@ function add_Entery(){
     });
 }
 add_Entery()
+
+let total_Price = document.querySelector('.total')
+function total(price){
+    let t=0;
+    // console.log(price);
+    price.forEach((ele) =>{
+        t+=parseInt(ele.value)
+        
+    })
+    // console.log(t);
+    total_Price.innerText=`Total: ${t}`
+
+}
