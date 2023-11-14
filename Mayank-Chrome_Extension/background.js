230f55ae4853fc112cf3228b21f8069e605253eb
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-11-15 01:58:15 +0530

    background issue resolved

--- Mayank-Chrome_Extension/background.js
+++ Mayank-Chrome_Extension/background.js
@@ -1,7 +1,8 @@
-chrome.tabs.onUpdated.addListener(async (tabId, tab) => {
-  let e = await chrome.tabs.query({active: true, lastFocusedWindow: true})
-  if (e[0].url && e[0].url.includes("youtube.com/watch")) {
-    const queryParameters = e[0].url.split("?")[1];
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  // console.log(tabId, changeInfo, tab);
+  // let e = await chrome.tabs.query({active: true, lastFocusedWindow: true})
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
     console.log(urlParameters);
 
