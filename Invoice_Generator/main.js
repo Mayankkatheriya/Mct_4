3531193d444b349685fc2e96e70d97cfb8edf819
Dibyas98 <dibyashaktip98.com>
2023-11-14 14:36:54 +0530

    print page done

--- Invoice_Generator/main.js
+++ Invoice_Generator/main.js
@@ -1,93 +1,143 @@
 let bill = document.querySelectorAll('.bill');
-let first= document.querySelector('.page')
-let c=2;
+let first = document.querySelector('.page')
+let total_Price = document.querySelector('.total')
+let print = document.querySelector('.print');
 // console.log(first);
 // console.log(bill);
 
 
 
-function shift_Price(){
-    let name=document.querySelectorAll('.name');
-    name.forEach((ele) =>{
-        ele.addEventListener('keyup',function(e){
-            if(e.key==='Enter' && ele.value.length>0){
+function shift_Price() {
+    let name = document.querySelectorAll('.name');
+    name.forEach((ele) => {
+        ele.addEventListener('keyup', function (e) {
+            if (e.key === 'Enter' && ele.value.length > 0) {
                 ele.parentElement.children[2].focus();
             }
-            
+
         })
     })
 }
 shift_Price();
 
-function add_Entery(){
-    let price=document.querySelectorAll('.price');
+function add_Entery() {
+    let price = document.querySelectorAll('.price');
     price.forEach(element => {
-        element.addEventListener('keyup',function(e){
-            if(e.key === 'Enter' && element.parentElement.children[1].value.length==0){
+        element.addEventListener('keyup', function (e) {
+            if (e.key === 'Enter' && element.parentElement.children[1].value.length == 0) {
                 alert('Enter prododut name');
             }
-            else if(e.key === 'Enter' && element.value.length>0){
-                element.disabled=true
-                let newDiv=document.createElement('li')
-                newDiv.classList.add('bill')
-            newDiv.innerHTML=`
-            <p>${c}</p>
-            <input type="text"  class="name">
-            <input type="number" class="price">
-            `
-            c++;
-            first.append(newDiv)
-            let l=first.children.length
-            first.children[l-1].children[1].focus();
-            // console.log(first.children[l-1].children[1]);
-            // console.log(first.children.length);
-            total(price);
-            add_Entery();
-            shift_Price();
+            else if (e.key === 'Enter' && element.value.length > 0) {
+                new_listAppend();
 
             }
-            
+
         })
     });
 }
 add_Entery()
 
-let total_Price = document.querySelector('.total')
-function total(price){
-    let t=0;
+
+function total(price) {
+    bill = document.querySelectorAll('.bill');
+    let t = 0;
     // console.log(price);
-    price.forEach((ele) =>{
-        t+=parseInt(ele.value)
-        
+
+    price.forEach((ele) => {
+        t += parseInt(ele.value)
+
     })
     // console.log(t);
-    total_Price.innerText=`Total: ${t}`
+    total_Price.innerHTML = `Total: ${t}
+    <span class="items">items:${bill.length-1}</span>
+    `
+    delete_Items();
+    
 
 }
-let print =document.querySelector('.print');
-print.addEventListener('click',function(){
-    print.style.display='none'
-    
-    let inp =document.querySelectorAll('input');
-    
+
+print.addEventListener('click', function () {
+    print.style.display = 'none'
+
+    let del = document.querySelectorAll('.delete');
+    // console.log(del);
+    del.forEach(element => {
+        element.classList.add('del-non')
+    })
+
+    let bill = document.querySelectorAll('.bill');
+
+    console.log(bill[bill.length - 1]);
+    bill[bill.length - 1].remove()
+
+
+    bill.forEach((ele) => {
+        ele.classList.add('list-whilePrint')
+    })
+
+
+    total_Price.classList.add('while-print')
+
     window.print();
-    
+    new_listAppend();
+    del.forEach(element => {
+        element.classList.remove('del-non')
+    })
+    total_Price.classList.remove('while-print')
+    print.style.display = 'block'
+    bill.forEach((ele) => {
+        ele.classList.remove('list-whilePrint')
+    })
+
+
 
 });
 
-    
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-let date =new Date();
-let time=date.toTimeString().slice(0,8)
-let month= date.getMonth()
-let dat= date.getDate()
-let year=date.getFullYear();
+
+
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let date = new Date();
+let time = date.toTimeString().slice(0, 8)
+let month = date.getMonth()
+let dat = date.getDate()
+let year = date.getFullYear();
 let day = weekday[date.getDay()];
 
-console.log(time,month,dat,year,day);
+// console.log(time,month,dat,year,day);
 
-let tim =document.querySelector('.date');
-tim.innerHTML=`
+let tim = document.querySelector('.date');
+tim.innerHTML = `
     Time: ${time} (${day})
     <p>Date: ${dat}-${month}-${year}</p>
 `
+function delete_Items() {
+    let del = document.querySelectorAll('.delete');
+    del.forEach((ele) => {
+        ele.addEventListener('click', function (e) {
+            e.target.parentElement.remove();
+        })
+    })
+    // total()
+}
+delete_Items();
+function new_listAppend() {
+    let price = document.querySelectorAll('.price');
+    let newDiv = document.createElement('li')
+    newDiv.classList.add('bill')
+    newDiv.innerHTML = `
+            <p>*</p>
+            <input type="text"  class="name">
+            <input type="number" class="price">
+            <i class="fa-solid fa-trash delete"></i>
+            `
+
+    first.append(newDiv)
+    let l = first.children.length
+    first.children[l - 1].children[1].focus();
+    // console.log(first.children[l-1].children[1]);
+    // console.log(first.children.length);
+    total(price);
+    add_Entery();
+    shift_Price();
+    print.style.display = 'block'
+}
