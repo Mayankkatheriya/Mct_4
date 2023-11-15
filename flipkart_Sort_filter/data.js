29785736980fba34ea288fd29994298d4fbf11eb
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-11-15 23:58:33 +0530

    flipkart added

---
+++ flipkart_Sort_filter/data.js
@@ -0,0 +1,466 @@
+const products = [
+
+    {
+    
+    image: "product1.jpg",
+    
+    title: "Product 1",
+    
+    rating: 4.5,
+    
+    flipkartAssured: true,
+    
+    price: 499.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 449.99,
+    
+    discountPrice: 50.00,
+    
+    popularity: 200,
+    
+    },
+    
+    {
+    
+    image: "product2.jpg",
+    
+    title: "Product 2",
+    
+    rating: 4.2,
+    
+    flipkartAssured: false,
+    
+    price: 299.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 279.99,
+    
+    discountPrice: 20.00,
+    popularity: 100,
+    
+    
+    },
+    
+    {
+    
+    image: "product3.jpg",
+    
+    title: "Product 3",
+    
+    rating: 3.9,
+    
+    flipkartAssured: true,
+    
+    price: 699.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 599.99,
+    
+    discountPrice: 100.00,
+    popularity: 500,
+    
+    
+    },
+    
+    {
+    
+    image: "product4.jpg",
+    
+    title: "Product 4",
+    
+    rating: 4.7,
+    
+    flipkartAssured: true,
+    
+    price: 199.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 179.99,
+    
+    discountPrice: 20.00,
+    popularity: 900,
+    
+    
+    },
+    
+    {
+    
+    image: "product5.jpg",
+    
+    title: "Product 5",
+    
+    rating: 4.3,
+    
+    flipkartAssured: true,
+    
+    price: 899.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 849.99,
+    
+    discountPrice: 50.00,
+    popularity: 10,
+    },
+    
+    {
+    
+    image: "product6.jpg",
+    
+    title: "Product 6",
+    
+    rating: 4.0,
+    
+    flipkartAssured: false,
+    
+    price: 399.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 359.99,
+    
+    discountPrice: 40.00,
+    
+    popularity: 50,
+    
+    },
+    
+    {
+    
+    image: "product7.jpg",
+    
+    title: "Product 7",
+    
+    rating: 4.8,
+    
+    flipkartAssured: true,
+    
+    price: 599.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 549.99,
+    
+    discountPrice: 50.00,
+    popularity: 1000,
+    
+    },
+    
+    {
+    
+    image: "product8.jpg",
+    
+    title: "Product 8",
+    
+    rating: 4.1,
+    
+    flipkartAssured: true,
+    
+    price: 349.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: false,
+    
+    specialPrice: 329.99,
+    
+    discountPrice: 20.00,
+    popularity: 2,
+    
+    },
+    
+    {
+    
+    image: "product9.jpg",
+    
+    title: "Product 9",
+    
+    rating: 3.8,
+    
+    flipkartAssured: true,
+    
+    price: 799.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 749.99,
+    
+    discountPrice: 50.00,
+    popularity: 34634,
+    
+    },
+    
+    {
+    
+    image: "product10.jpg",
+    
+    title: "Product 10",
+    
+    rating: 4.6,
+    
+    flipkartAssured: false,
+    
+    price: 249.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 229.99,
+    
+    discountPrice: 20.00,
+    popularity: 9800,
+    },
+    
+    {
+    
+    image: "product11.jpg",
+    
+    title: "Product 11",
+    
+    rating: 4.4,
+    
+    flipkartAssured: true,
+    
+    price: 599.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 579.99,
+    
+    discountPrice: 20.00,
+    popularity: 201,
+    
+    },
+    
+    {
+    
+    image: "product12.jpg",
+    
+    title: "Product 12",
+    
+    rating: 4.9,
+    
+    flipkartAssured: true,
+    
+    price: 179.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 169.99,
+    
+    discountPrice: 10.00,
+    popularity: 199,
+    
+    },
+    
+    {
+    
+    image: "product13.jpg",
+    
+    title: "Product 13",
+    
+    rating: 4.2,
+    
+    flipkartAssured: true,
+    
+    price: 899.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 849.99,
+    
+    discountPrice: 50.00,
+    popularity: 2300,
+    
+    },
+    
+    {
+    
+    image: "product14.jpg",
+    
+    title: "Product 14",
+    
+    rating: 4.3,
+    
+    flipkartAssured: false,
+    
+    price: 449.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 429.99,
+    
+    discountPrice: 20.00,
+    popularity: 990,
+    
+    },
+    
+    {
+    
+    image: "product15.jpg",
+    
+    title: "Product 15",
+    
+    rating: 4.7,
+    
+    flipkartAssured: true,
+    
+    price: 299.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 269.99,
+    
+    discountPrice: 30.00,
+    popularity: 1000,
+    
+    
+    },
+    
+    {
+    
+    image: "product16.jpg",
+    
+    title: "Product 16",
+    
+    rating: 4.0,
+    
+    flipkartAssured: true,
+    
+    price: 699.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 649.99,
+    
+    discountPrice: 50.00,
+    popularity: 0,
+    },
+    
+    {
+    
+    image: "product17.jpg",
+    
+    title: "Product 17",
+    
+    rating: 3.9,
+    
+    flipkartAssured: true,
+    
+    price: 549.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 499.99,
+    
+    discountPrice: 50.00,
+    popularity: 240,
+    
+    },
+    
+    {
+    
+    image: "product18.jpg",
+    
+    title: "Product 18",
+    
+    rating: 4.5,
+    
+    flipkartAssured: false,
+    
+    price: 499.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 469.99,
+    
+    discountPrice: 30.00,
+    popularity: 560,
+    
+    },
+    
+    {
+    
+    image: "product19.jpg",
+    
+    title: "Product 19",
+    
+    rating: 4.6,
+    
+    flipkartAssured: true,
+    
+    price: 349.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 329.99,
+    
+    discountPrice: 20.00,
+    popularity: 299,
+    
+    },
+    
+    {
+    
+    image: "product20.jpg",
+    
+    title: "Product 20",
+    
+    rating: 3.8,
+    
+    flipkartAssured: true,
+    
+    price: 649.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 599.99,
+    
+    discountPrice: 50.00,
+    popularity: 200,
+    
+    },
+    
+    ];
